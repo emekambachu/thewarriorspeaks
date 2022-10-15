723ea2ea02fa0320af5e161582fd3960f3602ae8
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-10-15 08:44:28 +0100

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+mix.setPublicPath('public_html');
+
+mix.js('resources/js/app.js', 'public_html/js').vue()
+    .sass('resources/sass/app.scss', 'public_html/css');
